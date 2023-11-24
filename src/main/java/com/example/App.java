adb81f47526b6f1fff5f0b7caf19ee347116f3e0
Karlson54 <anruban2001@gmail.com>
2023-11-24 22:38:37 +0200

    adding @Before to test

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,17 +1,15 @@
 package com.example;
 
 public class App {
-
-    public static boolean isPalindrome(String word) {
+    public boolean isPalindrome(String word) {
         String cleanedWord = word.replaceAll("\\s", "").toLowerCase();
-
         StringBuilder reversedWord = new StringBuilder(cleanedWord).reverse();
-
         return cleanedWord.equals(reversedWord.toString());
     }
 
     public static void main(String[] args) {
+        App app = new App();
         String exampleWord = "level";
-        System.out.println("Is '" + exampleWord + "' a palindrome? " + isPalindrome(exampleWord));
+        System.out.println("Is '" + exampleWord + "' a palindrome? " + app.isPalindrome(exampleWord));
     }
 }
--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,17 +1,25 @@
 package com.example;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 
 public class AppTest {
 
+    private App app;
+
+    @Before
+    public void setUp() {
+        app = new App();
+    }
+
     @Test
     public void testIsPalindrome() {
-        assertTrue(App.isPalindrome("level"));
-        assertTrue(App.isPalindrome("A man a plan a canal Panama"));
-        assertTrue(App.isPalindrome("Able was I saw eLba"));
-        assertFalse(App.isPalindrome("hello"));
-        assertFalse(App.isPalindrome("not a palindrome"));
+        assertTrue(app.isPalindrome("level"));
+        assertTrue(app.isPalindrome("A man a plan a canal Panama"));
+        assertTrue(app.isPalindrome("Able was I saw eLba"));
+        assertFalse(app.isPalindrome("hello"));
+        assertFalse(app.isPalindrome("not a palindrome"));
     }
 }
